1f846e1d801099f893d1b94ab2cbbe08965c7ab5
Mar <msanco2@icloud.com>
2023-03-01 20:38:42 -0700

    bmi calc complete

--- calculator.js
+++ calculator.js
@@ -10,6 +10,10 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmi", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function(req, res) {
 
     var num1 = Number(req.body.num1);
@@ -17,8 +21,17 @@ app.post("/", function(req, res) {
     var result = num1 + num2;
 
     res.send(num1 + " + " + num2 + " is equal to " + result + ".");
-})
+});
+
+app.post("/bmi", function(req, res) {
+
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = weight / Math.pow(height, 2);
+
+    res.send("Your BMI is " + bmi + ".");
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
-})
+});
